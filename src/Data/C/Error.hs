a48b347e7f86e5024d82ccd8d364b38294ff34ed
Sibi Prabakaran <sibi@psibi.in>
2016-11-02 14:32:20 +0530

    Hindent format

--- src/Data/C/Error.hs
+++ src/Data/C/Error.hs
@@ -1,4 +1,4 @@
-{-#LANGUAGE ForeignFunctionInterface#-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 
 module Data.C.Error where
 
@@ -8,8 +8,8 @@ import Foreign.C.String
 import Foreign.Marshal.Alloc (free)
 import System.IO.Unsafe (unsafePerformIO)
 
-foreign import ccall unsafe "stdio.h perror"
-        c_perror :: CString -> IO ()
+foreign import ccall unsafe "stdio.h perror" c_perror ::
+               CString -> IO ()
 
 perror :: String -> IO ()
 perror str = do
@@ -17,21 +17,25 @@ perror str = do
   c_perror str'
   free str'
 
-foreign import ccall unsafe "string.h strerror"
-        c_strerror :: CInt -> IO CString
+foreign import ccall unsafe "string.h strerror" c_strerror ::
+               CInt -> IO CString
 
 strerror :: Int -> String
-strerror x = unsafePerformIO $ do
-               cstr <- c_strerror (fromIntegral x)
-               str <- peekCString cstr
-               return str
+strerror x =
+  unsafePerformIO $
+  do cstr <- c_strerror (fromIntegral x)
+     str <- peekCString cstr
+     return str
 
-foreign import ccall unsafe "string.h strerror_r"
-        c_strerror_r :: CInt -> CString -> CSize -> IO CString
+foreign import ccall unsafe "string.h strerror_r" c_strerror_r ::
+               CInt -> CString -> CSize -> IO CString
 
 strerror_r :: Int -> Word64 -> String
-strerror_r errnum size = unsafePerformIO $ do
-                           cstr <- allocaBytes (fromIntegral size) (\ptr -> c_strerror_r (fromIntegral errnum) ptr (fromIntegral size))
-                           str <- peekCString cstr
-                           return str
-
+strerror_r errnum size =
+  unsafePerformIO $
+  do cstr <-
+       allocaBytes
+         (fromIntegral size)
+         (\ptr -> c_strerror_r (fromIntegral errnum) ptr (fromIntegral size))
+     str <- peekCString cstr
+     return str
