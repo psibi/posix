c9fc1a8e4e864b118cfdbc87858ea9f72f2be226
Sibi Prabakaran <sibi@psibi.in>
2016-11-05 05:53:02 +0530

    hlint cleanup

--- src/Data/Posix/Error.hs
+++ src/Data/Posix/Error.hs
@@ -24,8 +24,7 @@ strerror :: Int -> String
 strerror x =
   unsafePerformIO $
   do cstr <- c_strerror (fromIntegral x)
-     str <- peekCString cstr
-     return str
+     peekCString cstr
 
 foreign import ccall unsafe "string.h strerror_r" c_strerror_r ::
                CInt -> CString -> CSize -> IO CString
@@ -37,5 +36,4 @@ strerror_r errnum size =
        allocaBytes
          (fromIntegral size)
          (\ptr -> c_strerror_r (fromIntegral errnum) ptr (fromIntegral size))
-     str <- peekCString cstr
-     return str
+     peekCString cstr
