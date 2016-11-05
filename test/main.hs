88a2ad741502f628c691ad0071ae857fa9100dd3
Sibi Prabakaran <sibi@psibi.in>
2016-11-05 05:49:16 +0530

    Fix test script

--- test/main.hs
+++ test/main.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.C.File.Constant
-import qualified Data.C.File.IO as IO
+import Data.Posix.File.Constant
+import qualified Data.Posix.File.IO as IO
+import System.Posix.Types (CMode(..))
 import Data.ByteString
+import Data.Bits
 import Test.Hspec
 
 main :: IO ()
@@ -14,3 +16,16 @@ main =
                dat <- IO.read fd 30 30
                IO.close fd
                dat `shouldBe` ("This is a sample file.\n", 23)
+          it "readMode example" $
+            do fd <- IO.openMode "./test/sampleFile.txt" o_rdonly (CMode 0600)
+               dat <- IO.read fd 30 30
+               IO.close fd
+               dat `shouldBe` ("This is a sample file.\n", 23)
+          it "write example" $
+            do fd <- IO.open "./test/sample.txt" (o_wronly .|. o_trunc)
+               IO.write fd "hello world"
+               IO.close fd
+               fd <- IO.open "./test/sample.txt" (o_rdonly)
+               dat <- IO.read fd 30 30
+               IO.close fd
+               (fst dat) `shouldBe` "hello world"
